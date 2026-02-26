e613608dbd901c7879d90b93aa4b8879a9298ff4
Max <maxdarosa561@gmail.com>
2026-02-26 15:38:38 -0300

    Updated
    I want to set the results of negative roots

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -17,6 +17,7 @@ else:
         delta = (b ** 2) - (4 * a * c)
 
         if delta < 0:
+            #Here I want to set a system which you can see the negative roots
             print("Negative discriminant, there are no real roots.")
         else:
             x1 = (-b + math.sqrt(delta)) / (2 * a)
@@ -29,4 +30,5 @@ else:
             break
         else:
             print("- - -")
+
 
