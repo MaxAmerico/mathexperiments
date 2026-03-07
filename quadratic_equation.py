9a0a2c7d70929289092e51777b1e78e348c01ed1
Max <maxdarosa561@gmail.com>
2026-03-07 14:42:47 -0300

    Add: Functions

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,35 +1,43 @@
 import math
-
-print("Here is the quadratic equation model below:")
-print("ax² + bx + c = 0")
-print("Please replace the values above with the ones from your problem right after the prompt.")
-proceed = input("Can we proceed? (Y/N) ").upper()
-
-if proceed == "N":
-    print("Restart the program if you change your mind.")
-
-else:
-    while True:
-        a = float(input("What is the value of A? "))
-        b = float(input("What is the value of B? "))
-        c = float(input("What is the value of C? "))
-
+#Functions
+def calcule_quadratic(a,b,c):
         delta = (b ** 2) - (4 * a * c)
 
         if delta < 0:
             #Here I want to set a system which you can see the negative roots
-            print("Negative discriminant, there are no real roots.")
+            return print("Negative discriminant, there are no real roots.")
         else:
             x1 = (-b - math.sqrt(delta)) / (2 * a)
             x2 = (-b + math.sqrt(delta)) / (2 * a)
+            return  print(f"The roots are: x1 = {x1} and x2 = {x2}")
 
-            print(f"The roots are: x1 = {x1} and x2 = {x2}")
 
+def main_code():
+      while True:
+        a = float(input("What is the value of A?: "))
+        b = float(input("What is the value of B?: "))
+        c = float(input("What is the value of C?: "))
+   
+        r = calcule_quadratic(a, b, c)
+           
         question = input("Do you want to calculate other equations? (Y/N) ").upper()
-        if question == "N":
-            break
+        if question == 'Y':
+             continue
         else:
-            print("- - -")
+             break
+        
+          
+            
+#Finish of functions
+            
+#Start of the code        
+
+print("Here is the quadratic equation model below:")
+print("ax² + bx + c = 0")
+print("Please replace the values above with the ones from your problem right after the prompt.")
+
+code = main_code()
+
 
 
 
